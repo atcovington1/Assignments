2ac79594c859dc6ba8cc12946f7207409d9a5880
Aisha Covington <atcovington1@gmail.com>
2016-08-19 15:04:36 -0400

    resubmit Simple Crap Game

--- Day 3/PlaySimpleCraps.java
+++ Day 3/PlaySimpleCraps.java
@@ -1,4 +1,4 @@
-package Assignments;
+package ssa;
 
 public class PlaySimpleCraps {
 	
@@ -10,15 +10,16 @@ public class PlaySimpleCraps {
 		int dice1 = rnd.nextInt(6)+1;
 		int dice2 = rnd.nextInt(6)+1;
 		int roll = dice1 + dice2;
+		int thePoint = roll;
 		
 		System.out.println();
 		
-		System.out.print("You rolled "+roll+". ");
-		
+		System.out.println("You rolled "+ roll +". ");
+
 		if(roll == 2 || roll == 3 || roll == 12){
 			System.out.println("First Time Loser!");
 		
-		}else if(roll == 7 || roll == 11){
+		}else if(roll == 7){
 			System.out.println("All You Do Is Win, Win,Win !");
 		
 			
@@ -26,26 +27,23 @@ public class PlaySimpleCraps {
 			System.out.println("Point Roll is "+roll+"\n");
 			dice1 = rnd.nextInt(6)+1;
 			dice2 = rnd.nextInt(6)+1;
-			int roll2 = dice1 + dice2;
-			System.out.print("You rolled "+roll2+". ");
-			while(roll2 != 7){
-				if(roll == roll2){
-					System.out.println("Winner, Winner, Chicken Dinner!");
-		
+			roll = dice1 + dice2;
+			
+			System.out.println("You rolled "+ roll +". ");
+			while(thePoint != roll && 7!= roll){
 
 				
-				}else{
-					System.out.println("Point Roll is "+roll+"\n");
-				}
 				dice1 = rnd.nextInt(6)+1;
 				dice2 = rnd.nextInt(6)+1;
-				roll2 = dice1 + dice2;
-				System.out.print("You rolled "+roll2+". ");
+				roll = dice1 + dice2;
+				System.out.println("You rolled "+ roll +". ");
 			}
 			
-			if(roll2 == 7){
+			if(roll == 7){
 				System.out.println("You Lose, Try Again!");
-			}			
+			} if (thePoint == roll){
+				System.out.println("Winner, Winner, Chicken Dinner");
+			}
 		}
 	}
 }

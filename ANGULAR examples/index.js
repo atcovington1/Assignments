037a8e3bbec9a43fc8ee9874bcae0e9f64a05321
Aisha Covington <atcovington1@gmail.com>
2016-10-05 16:34:02 -0400

    Day38

---
+++ ANGULAR examples/index.js
@@ -0,0 +1,49 @@
+angular.module("AppMod", ["ngRoute"])
+	.controller("AppCtrl", ['$http', function($http) {
+		var self = this;
+		self.hide = function(x){
+			
+				for(var std of self.students) {
+					if(std.id==x){
+					std.vis = false;
+				}
+			}
+		};
+		self.refresh = function(){
+		for(var std of self.students) {
+				
+					std.vis = true;
+				}
+
+	};
+
+		$http.get('http://localhost:8080/student')
+			.then(function(resp){
+				self.students = resp.data;
+				for(var s of self.students) {
+					s.vis = true;
+				}
+				console.log(self.students);
+			},function(err) {
+
+			});
+
+	}])
+	.config(['$routeProvider', function($routeProvider) {
+
+   		$routeProvider
+		.when('/', {
+			templateUrl: 'views/home.view.html'
+
+		}).when('/student', {
+			templateUrl: 'views/student.view.html',
+			controller: 'AppCtrl',
+			controllerAs: 'ctrl'
+
+		}).when('/about', {
+			templateUrl: 'views/about.view.html'
+
+		})
+		.otherwise({redirectTo: '/'});
+
+	}]); // end config
